459dc8e7f5effd597655b5f484e37c5dbbcca266
paulheisele@gmail.com <paulheisele@gmail.com>
2018-08-05 23:03:17 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,8 +3,35 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(name:, breed:)
+  def initialize(name:, breed:, id: nil)
     @name = name
     @breed = breed
     @id = id
   end
+
+  def self.create_table
+
+  end
+
+  def self.drop_table
+
+  end
+
+  def self.new_from_db
+
+  end
+
+
+  def self.find_by_name
+
+  end
+
+  def save
+
+  end
+
+  def update
+
+  end 
+
+  
